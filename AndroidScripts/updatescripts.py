b088e74a81c3c43a118f244fa33c1ae2e257af96
Mili <Mili-NT@users.noreply.github.com>
2021-07-09 16:42:30 -0400

    added python scripts

--- AndroidScripts/updatescripts.py
+++ AndroidScripts/updatescripts.py
@@ -1,1 +1,10 @@
 #!/bin/python3
+import os
+
+if __name__ == '__main__':
+    path = "/data/data/com.termux/files/home/bin/"
+    url = "https://mili-nt.github.io/AndroidScripts/"
+    os.system(f"rm {path}*")
+    os.system(f"wget https://mili-nt.github.io/AndroidScripts/termux-url-opener -P {path}")
+    os.system(f"wget https://mili-nt.github.io/AndroidScripts/termux-file-editor -P {path}")
+    os.system(f"chmod +x {path}*")
