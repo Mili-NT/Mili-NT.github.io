e78f20d819d52f3ae827c632b12212c0bb4acd72
Mili <Mili-NT@users.noreply.github.com>
2021-07-09 21:22:53 -0400

    added nano alias

--- AndroidScripts/appsetup.sh
+++ AndroidScripts/appsetup.sh
@@ -5,5 +5,6 @@ mkdir -p ~/.config/youtube-dl
 echo "# Default Output Directory and Pattern" > ~/.config/youtube-dl/config
 echo "-o ~/storage/downloads/%(extractor_key)s/%(uploader)s/%(title)s-%(id)s.%(ext)s" >> ~/.config/youtube-dl/config
 mkdir ~/bin
+echo 'alias nano="nano -T 4 $@"' >> /data/data/com.termux/files/usr/etc/bash.bashrc && bash
 wget https://mili-nt.github.io/AndroidScripts/updatescripts.py -P ~/
 python3 ~/updatescripts.py
