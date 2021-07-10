22376ee606bb4a6fae1c2a6ad4ed725dd3185114
Mili <Mili-NT@users.noreply.github.com>
2021-07-09 22:16:20 -0400

    added dependencies

--- AndroidScripts/appsetup.sh
+++ AndroidScripts/appsetup.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 termux-setup-storage
 apt update && apt upgrade -y
-pkg install -y wget && pkg install -y python && apt install -y python ffmpeg && pip install youtube-dl
+pkg install -y wget && pkg install -y python && apt install -y python ffmpeg && pip install youtube-dl && pip install b4 && pip install requests
 mkdir -p ~/.config/youtube-dl
 echo "# Default Output Directory and Pattern" > ~/.config/youtube-dl/config
 echo "-o ~/storage/downloads/%(extractor_key)s/%(uploader)s/%(title)s-%(id)s.%(ext)s" >> ~/.config/youtube-dl/config
