5bb57af61c976dc2beac5ae3cc75d3d977c37cc5
Mili <Mili-NT@users.noreply.github.com>
2021-07-09 20:18:29 -0400

    Changed setup script to a shell script

---
+++ AndroidScripts/appsetup.sh
@@ -0,0 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/bash
+apt update && apt upgrade && pkg install wget && pkg install python &&e apt install python ffmpeg && pip install youtube-dl
+mkdir -p ~/.config/youtube-dl
+echo "# Default Output Directory and Pattern" > ~/.config/youtube-dl/config
+echo "-o ~/storage/downloads/%(extractor_key)s/%(uploader)s/%(title)s-%(id)s.%(ext)s" >> ~/.config/youtube-dl/config
+mkdir ~/bin
+wget https://mili-nt.github.io/AndroidScripts/updatescripts.py -P ~/
+python3 ~/updatescripts.py
